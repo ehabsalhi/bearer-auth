f66e9644f548c906afebe6fd0c5e091797f8ca42
ehabsalhi <ehab1salhe@gmail.com>
2023-06-26 13:39:55 +0000

    dev 5

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -17,7 +17,7 @@ describe('server test' , () => {
      })
 
      it('signup test' , async () =>{
-          const name = 'ehab_test108'
+          const name = 'ehab_test111'
           const res = await muke.post('/signup').send({
                username: name,
                password : '123123'
